510f6d3ef377cbb3b18ae37def91f8b5dcafd4fa
kimhue2003 <125335147+kimhue2003@users.noreply.github.com>
2023-02-14 11:20:59 +0700

    test

---
+++ PhamThiHue_Project/src/main/java/com/mycompany/phamthihue_project/PhamThiHue_Project.java
@@ -0,0 +1,9 @@
+
+package com.mycompany.phamthihue_project;
+
+public class PhamThiHue_Project {
+
+    public static void main(String[] args) {
+        System.out.println("Phạm Thị Huệ học java");
+    }
+}
